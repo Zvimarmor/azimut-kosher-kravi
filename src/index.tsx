fd8961e818662a9fcbd5377304bfd3d8df10eb63
Zvimarmor <zvi.marmor@mail.huji.ac.il>
2025-10-27 16:20:56 +0200

    Expose logger to window object for console debugging
    Now you can use these commands in browser console:
    - logger.printLogs() - Show all persisted logs
    - logger.clearLogs() - Clear all logs
    - logger.getLogs() - Get raw log array
    
    This makes debugging auth redirects much easier since
    logger is globally accessible in the console.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { logger } from './lib/utils/logger'
+
+// Expose logger to window for debugging in console
+declare global {
+  interface Window {
+    logger: typeof logger;
+  }
+}
+
+window.logger = logger;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
